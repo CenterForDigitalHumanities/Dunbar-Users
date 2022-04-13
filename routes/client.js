3920a0a17ac9376f2b0177fcd16455af15727755
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2022-04-13 15:07:19 -0500

    Ha, so boss.  This now uses Auth0 to store and Authenticate users for 'Dunbar Users'.  When a user logs in, they get an access token an the client gets to the know the agent.  Username/Email comes from the agent.  When any dunbar connected app makes a request, they should use this access token as the Bearer.  This will let them 'be logged in' to dunbar.  They have a living access token that can be verified, and the agent for that token can be compared to the client known agent.

--- routes/client.js
+++ routes/client.js
@@ -1,13 +1,14 @@
 var express = require('express')
 var router = express.Router()
+const got = require('got')
 const auth = require('../auth')
 
-router.get('/register', function(req,res,next){
+router.get('/connect', function(req,res,next){
   //Register means become part of the user DB on Auth0, or be identified for login
       var params = new URLSearchParams({
           // "audience":process.env.AUDIENCE,
           // "scope":"openid name email offline_access",
-          //"scope":"name email openid offline_access",
+          "scope":"name email openid profile offline_access",
           //"response_type":"code",
           "response_type":"token",
           "client_id":process.env.CLIENTID,
@@ -16,5 +17,17 @@ router.get('/register', function(req,res,next){
       }).toString()
       res.status(200).send("https://cubap.auth0.com/authorize?" + params)
   })
+
+router.get('/dunbar-user', async function(req,res,next){
+  let user = null
+  if(req.query.access_token){
+    let user = await got.get("https://cubap.auth0.com/userinfo?access_token="+req.query.access_token).json()
+    res.status(200).json(user)
+    return user
+  }
+  else{
+    res.status(400).send("Your request does not contain an access token.")
+  }
+})
 
 module.exports = router
