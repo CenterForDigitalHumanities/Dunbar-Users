d075882f2428f4ecddadd623cc06ab16273f6474
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2022-04-14 14:33:13 -0500

    Phew, back out.  The realization is that your apps will either use auth0.js to login/logout/getinfo with your favorite auth0 app/api, or they will use the dunbar-users API to do that with the dunbar auth0 app/api.  That means dunbar-users is not a client and cannot act like a client.  It cannot use auth0.js or require(auth0) in the background.  All it can do is interact with the Auth0 Dunbar Users API with /authorize and /userinfo.

--- routes/client.js
+++ routes/client.js
@@ -1,41 +1,33 @@
+//Imagine this as a proxy piece of a client that other clients that are a part of this Organization can use
+//Dunbar Clients can call to this to be their auth0 client.  They should be able to login, logout and get their user profile. 
+
 var express = require('express')
 var router = express.Router()
 const got = require('got')
 const auth = require('../auth')
-const auth0 = require ('auth0')
-const AuthenticationClient = auth0.AuthenticationClient
-const ManagementClient = require('auth0').ManagementClient;
-
 
 router.get('/connect', function(req,res,next){
-  let authClient = new AuthenticationClient({
-    domain:  process.env.DOMAIN,
-    clientId:  process.env.CLIENTID,
-    clientSecret:  process.env.CLIENT_SECRET,
+  //Register means become part of the user DB on Auth0, or be identified for login
+  //This is a 'Social Login'
+      var params = new URLSearchParams({
+          "audience":process.env.AUDIENCE,
+          "scope":"name email openid profile offline_access",
+          "response_type":"token",
+          "client_id":process.env.CLIENTID,
+          "redirect_uri":process.env.DUNBAR_REDIRECT,
+          "state":"dunbar123"           
+      }).toString()
+      res.status(301).send("https://cubap.auth0.com/authorize?" + params)
   })
-  res.status(301) // maybe redirect?
-  return authClient.loginWithRedirect({
-          redirect_uri: "http://localhost:3000",
-          audience:process.env.AUDIENCE,
-          scope:"name email openid profile offline_access",
-          responseType:"token",
-          redirectUri:process.env.DUNBAR_REDIRECT,
-          state:"dunbar123"   
-  })
-})
 
 router.get('/disconnect', function(req,res,next){
-  let authClient = new AuthenticationClient({
-    domain:  process.env.DOMAIN,
-    clientId:  process.env.CLIENTID,
-    clientSecret:  process.env.CLIENT_SECRET    
+  //Logout this user
+      var params = new URLSearchParams({
+          "client_id":process.env.CLIENTID,
+          "returnTo":process.env.DUNBAR_REDIRECT      
+      }).toString()
+      res.status(301).send("https://cubap.auth0.com/v2/logout?" + params)
   })
-  res.status(301) // maybe redirect?
-  return authClient.logout({
-        returnTo: process.env.DUNBAR_REDIRECT,
-        client_id: process.env.CLIENTID
-    })
-})
 
 router.get('/dunbar-user', async function(req,res,next){
   let user = null
@@ -47,23 +39,6 @@ router.get('/dunbar-user', async function(req,res,next){
   else{
     res.status(400).send("Your request does not contain an access token.")
   }
-  /*
-  //Hmm there isn't anything equivalent to using the access token to get the user profile.  Maybe we don't want the access token, maybe an id token instead?
-  let user = null
-  if(req.query.access_token){
-    let managerClient = new ManagerClient({
-      domain:  process.env.DOMAIN,
-      token: req.query.access_token,   
-    })
-    managerClient.userInfo(req.query.access_token, function(err, u) {
-        res.status(200)
-        res.json(u)
-    })
-  }
-  else{
-    res.status(400).send("Your request does not contain an access token.")
-  }
-  */
 })
 
 module.exports = router
