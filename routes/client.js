b449e3f12efb2210750e41339164beb715103ccd
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2022-04-14 12:53:12 -0500

    Ok, but lets use the aut0.js in our module.  We will keep complete control over login, logout, and user info.

--- routes/client.js
+++ routes/client.js
@@ -2,26 +2,54 @@ var express = require('express')
 var router = express.Router()
 const got = require('got')
 const auth = require('../auth')
+const auth0 = require ('auth0')
+const AuthenticationClient = auth0.AuthenticationClient
 
 router.get('/connect', function(req,res,next){
-  //Register means become part of the user DB on Auth0, or be identified for login
-      var params = new URLSearchParams({
-          "audience":process.env.AUDIENCE,
-          "scope":"name email openid profile offline_access",
-          "response_type":"token",
-          "client_id":process.env.CLIENTID,
-          "redirect_uri":process.env.DUNBAR_REDIRECT,
-          "state":"dunbar123"           
-      }).toString()
-      res.status(200).send("https://cubap.auth0.com/authorize?" + params)
+  // let authClient = new AuthenticationClient({
+  //   domain:       process.env.DOMAIN,
+  //   clientID:     process.env.CLIENTID
+  // })
+  let webAuth = new auth0.WebAuth({
+      domain:       process.env.DOMAIN,
+      clientID:     process.env.CLIENTID
   })
+  // Trigger login, it will take you back to login.html with the access token
+  res.status(301) // maybe redirect?
+  return webAuth.authorize({
+      "audience":process.env.AUDIENCE,
+      "scope":"name email openid profile offline_access",
+      "responseType":"token",
+      "redirectUri":process.env.DUNBAR_REDIRECT,
+      "state":"dunbar123"      
+  })
+})
+
+router.get('/disconnect', function(req,res,next){
+  // let authClient = new AuthenticationClient({
+  //   domain:       process.env.DOMAIN,
+  //   clientID:     process.env.CLIENTID
+  // })
+  let webAuth = new auth0.WebAuth({
+      domain:       process.env.DOMAIN,
+      clientID:     process.env.CLIENTID
+  })
+  // Trigger login, it will take you back to login.html with the access token
+  res.status(301) // maybe redirect?
+  return webAuth.logout({
+        returnTo: process.env.DUNBAR_REDIRECT,
+        client_id: process.env.CLIENTID
+    })
+})
 
 router.get('/dunbar-user', async function(req,res,next){
   let user = null
   if(req.query.access_token){
-    let user = await got.get("https://cubap.auth0.com/userinfo?access_token="+req.query.access_token).json()
-    res.status(200).json(user)
-    return user
+    //We want Auth0 to give us the user info for this access token
+    webAuth.client.userInfo(getURLHash("access_token"), function(err, u) {
+        res.status(200)
+        res.json(u)
+    })
   }
   else{
     res.status(400).send("Your request does not contain an access token.")
