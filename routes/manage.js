79e6c52976c47d7afdbeb3159292cbe49a77f65a
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2022-04-27 16:09:00 -0500

    Use Universal Login to sign up a new user, then be returned as the admin to the user management page.

--- routes/manage.js
+++ routes/manage.js
@@ -7,7 +7,6 @@ const router = express.Router()
 const got = require('got')
 const auth = require('../auth')
 
-//Behind the scenes the Client Credentials Grant is used to obtain the access_token and is by default cached for the duration of the returned expires_in value
 let manager = new ManagementClient({
   domain: process.env.DOMAIN,
   clientId: process.env.CLIENTID,
@@ -15,19 +14,11 @@ let manager = new ManagementClient({
   scope: "create:users read:users read:user_idp_tokens update:users delete:users read:roles create:roles update:roles delete:roles"
 })
 
-//Behind the scenes the Client Credentials Grant is used to obtain the access_token and is by default cached for the duration of the returned expires_in value
 let authenticator = new AuthenticationClient({
   domain: process.env.DOMAIN,
   clientId: process.env.CLIENTID
 })
 
-function isAdmin(user){
-  let roles = {roles:[]}
-  if(user[process.env.DUNBAR_ROLES_CLAIM]){
-    roles = user[process.env.DUNBAR_ROLES_CLAIM].roles ?? {roles:[]}
-  }
-  return roles.includes(process.env.DUNBAR_ADMIN_ROLE)
-}
 
 /**
  * We trust this as our Auth0 server, because it's ours.
@@ -60,12 +51,52 @@ router.get('/getManagementToken', async function(req,res,next){
 
 /**
  * We trust this as our Auth0 server, because it's ours.
- * Ask our Dunbar Auth0 for a fresh access token with user management scope.
- * //FIXME!  The general access token or user should come in with this request.  Only fire if they are admin!
-  manager.getAccessToken()
+ * Tell our Dunbar Auth0 to assign the given user id to the Dunbar Public role.
+ * This limits access token scope.
+ * Other roles are removed.
  */ 
-router.get('/refreshManagementToken', async function(req,res,next){
-  res.status(200).send("Nothing Yet")
+router.get('/assignPublicRole/:_id', async function(req,res,next){
+  let token = req.header("Authorization") ?? ""
+  token = token.replace("Bearer ", "")
+  authenticator.getProfile(token)
+  .then(user =>{
+      if(isAdmin(user)){
+        let params =  { "id" : process.env.DUNBAR_PUBLIC_ROLE_ID}
+        let data = { "users" : [req.params._id]}
+        manager.users.assignUsersToRole(params, data)
+        .then(resp => {
+          if(resp.ok){
+            //unassign from other roles
+            params = {"id" : req.params._id}
+            data = { "roles" : [process.env.DUNBAR_CONTRIBUTOR_ROLE_ID, process.env.DUNBAR_ADMIN_ROLE_ID]}
+            manager.users.removeRolesFromUser(params, data)
+            .then(resp2 =>{
+              if(resp2.ok){
+                res.status(200).send("Public role was successfully assinged to the user")
+              }
+              else{
+                res.status(500).send("Internal error assigning role to use")
+              }
+            })
+            .catch(err => {
+              res.status(500).send(err)  
+            })  
+          }
+          else{
+            res.status(500).send("Internal error assigning role to user")
+          }
+        })
+        .catch(err => {
+          res.status(500).send(err)  
+        })
+      }
+      else{
+        res.status(500).send("You are not an admin")  
+      }
+  })
+  .catch(err => {
+    res.status(500).send(err)
+  })
 })
 
 /**
@@ -73,37 +104,55 @@ router.get('/refreshManagementToken', async function(req,res,next){
  * Tell our Dunbar Auth0 to assign the given user id to the Dunbar Public role.
  * This limits access token scope.
  * Other roles are removed.
- * //FIXME!  The general access token or user should come in with this request.  Only fire if they are admin!
-  manager.getAccessToken()
  */ 
-router.get('/assignPublicRole/:_id', async function(req,res,next){
-    const params =  { "id" : req.params["_id"]}
-    const data = { "roles" : [process.env.PUBLIC_ROLE_ID]}
-    manager.users.assignRolesToUser(params, data)
-    .then(resp => {
-      console.log("resp is")
-      console.log(resp)
-      if(resp.ok){
-         res.status(200).send("Public role was successfully assinged to the user")  
+router.post('/addUserToDunbar', async function(req,res,next){
+  let token = req.header("Authorization") ?? ""
+  token = token.replace("Bearer ", "")
+  authenticator.getProfile(token)
+  .then(async function(user){
+      if(isAdmin(user)){
+        let test = {
+          nickname: 'bryn',
+          name: 'bry@dunbar.io',
+          picture: 'https://s.gravatar.com/avatar/fc39f4f5b1c3381abe2ef2d25e79af3d?s=480&r=pg&d=https%3A%2F%2Fcenterfordigitalhumanities.github.io%2Frerum-consortium%2Flogo.png',
+          app_metadata:{
+            "agent" : "http://test.rerum.io/testing",
+            "app" : "dla"
+          }
+        }
+        let user = await management.createUser(test)
+        res.status(200).json(user)
       }
       else{
-        res.status(500).send("Internal error assigning role to use")
+        res.status(500).send("You are not an admin")  
       }
-    })
-    .catch(err => {
-      res.status(500).send(err)  
-    })
+  })
+  .catch(err => {
+    res.status(500).send(err)
+  })
+})
 
-    /*
-     manager.users.assignRolesToUser(params, data, function (err, user) {
-      if (err) {
-          // Handle error.
-          console.error(err)
-          res.status(500).json(err)
+/**
+ * We trust this as our Auth0 server, because it's ours.
+ * Tell our Dunbar Auth0 to assign the given user id to the Dunbar Public role.
+ * This limits access token scope.
+ * Other roles are removed.
+ */ 
+router.post('/removeUserFromDunbar', async function(req,res,next){
+  let token = req.header("Authorization") ?? ""
+  token = token.replace("Bearer ", "")
+  authenticator.getProfile(token)
+  .then(user =>{
+      if(isAdmin(user)){
+
       }
-      res.status(200).send("Public role was successfully assinged to the user")
-    })
-    */ 
+      else{
+        res.status(500).send("You are not an admin")  
+      }
+  })
+  .catch(err => {
+    res.status(500).send(err)
+  })
 })
 
 function getURLHash(variable, url) {
@@ -114,6 +163,14 @@ function getURLHash(variable, url) {
         if (pair[0] == variable) { return pair[1] }
     }
     return false
+}
+
+function isAdmin(user){
+  let roles = {roles:[]}
+  if(user[process.env.DUNBAR_ROLES_CLAIM]){
+    roles = user[process.env.DUNBAR_ROLES_CLAIM].roles ?? {roles:[]}
+  }
+  return roles.includes(process.env.DUNBAR_ADMIN_ROLE)
 }
 
 module.exports = router
