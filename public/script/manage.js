653715aca409b5e0c636a5e1584577bdc1a37834
cubap <cubap@slu.edu>
2023-02-21 16:05:09 -0600

    getting the user management page up and running

--- public/script/manage.js
+++ public/script/manage.js
@@ -1,25 +1,25 @@
 #!/usr/bin/env node
-import AuthButton from 'https://centerfordigitalhumanities.github.io/DLA/auth.js'
 const auth = document.querySelector('[is="auth-button"]')
+import jwt_decode from "/script/jwt.js"
 
-const AUDIENCE = "https://cubap.auth0.com/api/v2/"
-const CLIENTID = "z1DuwzGPYKmF7POW9LiAipO5MvKSDERM"
-const DUNBAR_REDIRECT = origin + "/manage.html"
-const DOMAIN = "cubap.auth0.com"
+// const AUDIENCE = "https://cubap.auth0.com/api/v2/"
+// const CLIENTID = "z1DuwzGPYKmF7POW9LiAipO5MvKSDERM"
+// const DUNBAR_REDIRECT = origin + "/manage.html"
+// const DOMAIN = "cubap.auth0.com"
 const DUNBAR_USER_ROLES_CLAIM = "http://dunbar.rerum.io/user_roles"
 const DUNBAR_PUBLIC_ROLE = "dunbar_user_public"
 const DUNBAR_CONTRIBUTOR_ROLE = "dunbar_user_contributor"
 const DUNBAR_ADMIN_ROLE = "dunbar_user_admin"
-const myURL = document.location.href
+// const myURL = document.location.href
 
-/**
- * Solely for getting the user profile.
- */
-let authenticator = new auth0.Authentication({
-    "domain": DOMAIN,
-    "clientID": CLIENTID,
-    "scope": "read:roles update:current_user_metadata read:current_user name nickname picture email profile openid offline_access"
-})
+// /**
+//  * Solely for getting the user profile.
+//  */
+// let authenticator = new auth0.Authentication({
+//     "domain": DOMAIN,
+//     "clientID": CLIENTID,
+//     "scope": "read:roles update:current_user_metadata read:current_user name nickname picture email profile openid offline_access"
+// })
 
 auth.addEventListener("dla-authenticated", ev => {
     const ref = getReferringPage()
@@ -46,133 +46,115 @@ auth.addEventListener("dla-authenticated", ev => {
     }
 })
 
-function adminOnly(token = window.DLA_USER?.authorization) {
+async function adminOnly(token = window.DLA_USER?.authorization) {
     //You can trust the token.  However, it may have expired.
     //A token was in localStorage, so there was a login during this window session.
     //An access token from login is stored. Let's use it to get THIS USER's info.  If it fails, the user needs to login again.
     try {
-        authenticator.userInfo(token, async (err, _u) => {
-            if (err) {
-                auth.login({
-                    "authParamsMap": { 'app': 'dla' },
-                    "scope": "read:roles update:current_user_metadata read:current_user name nickname picture email profile openid offline_access",
-                    "redirectUri": DUNBAR_REDIRECT,
-                    "responseType": "id_token token"
-                })
-                return
-            }
-            if (isAdmin(_u)) {
-                userList.innerHTML = ""
-                const user_arr = await getAllUsers()
-                let elem = ``
-                for (let user of user_arr) {
-                    //This presumes they will only have one dunbar role here.  Make sure getAllUsers() accounts for that.
-                    const role = user[DUNBAR_USER_ROLES_CLAIM]?.roles[0]?.replace("dunbar_user_", "") ?? "Role Not Assigned"
-                    elem += `<li user="${_u.name}">${user.name}
-            <em class="role" userid="${user.user_id}"> : ${role}</em>`
-                    if (role !== "Admin") {
-                        elem += `<div class="actions">
-                        <input class="tag is-small "bg-primary" 
-                            type="button" 
-                            value="Make ${role === "public" ? "Contributor" : "Public"}" 
-                            onclick="assignRole('${user.user_id}','${role === "public" ? 'Contributor' : 'Public'}')"/>
-                    </div>`
-                    }
-                    elem += `</li>
-            `
+        if (isAdmin(token)) {
+            userList.innerHTML = ""
+            const user_arr = await getAllUsers()
+            let elem = ``
+            for (let user of user_arr) {
+                //This presumes they will only have one dunbar role here.  Make sure getAllUsers() accounts for that.
+                const role = user[DUNBAR_USER_ROLES_CLAIM]?.roles[0]?.replace("dunbar_user_", "") ?? "Role Not Assigned"
+                elem += `<li user="${user.name}">${user.name}
+        <em class="role" userid="${user.user_id}"> : ${role}</em>`
+                if (role !== "Admin") {
+                    elem += `<div class="actions">
+                    <input class="tag is-small "bg-primary" 
+                        type="button" 
+                        value="Make ${role === "public" ? "Contributor" : "Public"}" 
+                        onclick="assignRole('${user.user_id}','${role === "public" ? 'Contributor' : 'Public'}')"/>
+                </div>`
                 }
-                userList.innerHTML += elem
+                elem += `</li>
+        `
             }
-        })
+            userList.innerHTML += elem
+        }
     } catch (_err) {
-        //auth0 crashing error
-        auth.login({
-            "authParamsMap": { 'app': 'dla' },
-            "scope": "read:roles update:current_user_metadata read:current_user name nickname picture email profile openid offline_access",
-            "redirectUri": DUNBAR_REDIRECT,
-            "responseType": "id_token token"
-        })
-        return
+        alert('not admin. boop.')
     }
     history.replaceState(null, null, ' ')
 }
 
-async function assignRole(userid, role) {
-    let url = `/dunbar-users/manage/assign${role}Role/${userid}`
-    document.querySelectorAll(`.role[userid="${userid}"]`).forEach(elem => {
-        elem.innerHTML = ` : Updating Role...`
-    })
-    fetch(url, {
-        method: 'GET',
-        cache: 'default',
-        headers: {
-            'Authorization': `Bearer ${window.DLA_USER?.authorization}`,
-            'Content-Type': "application/json; charset=utf-8"
-        }
-    })
-        .then(_resp => {
-            document.querySelectorAll(`.role[userid="${userid}"]`).forEach(elem => {
-                elem.innerHTML = ` : ${role}`
-            })
-        })
-        .catch(err => {
-            console.error("Role was not assigned")
-            console.error(err)
-            document.querySelectorAll(`.role[userid="${userid}"]`).forEach(elem => {
-                elem.innerHTML = ` : Error`
-            })
-        })
-}
+// async function assignRole(userid, role) {
+//     let url = `/dunbar-users/manage/assign${role}Role/${userid}`
+//     document.querySelectorAll(`.role[userid="${userid}"]`).forEach(elem => {
+//         elem.innerHTML = ` : Updating Role...`
+//     })
+//     fetch(url, {
+//         method: 'GET',
+//         cache: 'default',
+//         headers: {
+//             'Authorization': `Bearer ${window.DLA_USER?.authorization}`,
+//             'Content-Type': "application/json; charset=utf-8"
+//         }
+//     })
+//         .then(_resp => {
+//             document.querySelectorAll(`.role[userid="${userid}"]`).forEach(elem => {
+//                 elem.innerHTML = ` : ${role}`
+//             })
+//         })
+//         .catch(err => {
+//             console.error("Role was not assigned")
+//             console.error(err)
+//             document.querySelectorAll(`.role[userid="${userid}"]`).forEach(elem => {
+//                 elem.innerHTML = ` : Error`
+//             })
+//         })
+// }
 
-/**
- * PUT to the dunbar-users back end.
- * You must supply your login token in the Authorization header.
- * The body needs to be a user object, and you need to supply the user id in the body.
- * You can only update the user info belonging to the user encoded on the token in the Authorization header
- * This means you can only do this to update "your own" profile information.
- */
-async function updateUserInfo(event, userid=window.DLA_USER?.sub) {
-    event.preventDefault()
-    let info = new FormData(event.target)
-    let data = Object.fromEntries(info.entries())
-    for (let prop in data) {
-        if (data[prop] === "" || data[prop] === null || data[prop] === undefined) {
-            delete data[prop]
-        }
-    }
-    data.user_id = userid
-        let updatedUser = await fetch("/dunbar-users/manage/updateProfileInfo", {
-            method: 'PUT',
-            cache: 'default',
-            headers: {
-                'Authorization': `Bearer ${window.DLA_USER?.authorization}`,
-                'Content-Type': "application/json; charset=utf-8"
-            },
-            body: JSON.stringify(data)
-        })
-            .then(r => r.json())
-            .catch(err => {
-                console.error("User Not Updated")
-                console.error(err)
-                return {}
-            })
-        if (updatedUser.user_id) {
-            alert("User Info Updated!")
-        }
-        else {
-            alert("User Info Update Failed!")
-        }
-}
+// /**
+//  * PUT to the dunbar-users back end.
+//  * You must supply your login token in the Authorization header.
+//  * The body needs to be a user object, and you need to supply the user id in the body.
+//  * You can only update the user info belonging to the user encoded on the token in the Authorization header
+//  * This means you can only do this to update "your own" profile information.
+//  */
+// async function updateUserInfo(event, userid=window.DLA_USER?.sub) {
+//     event.preventDefault()
+//     let info = new FormData(event.target)
+//     let data = Object.fromEntries(info.entries())
+//     for (let prop in data) {
+//         if (data[prop] === "" || data[prop] === null || data[prop] === undefined) {
+//             delete data[prop]
+//         }
+//     }
+//     data.user_id = userid
+//         let updatedUser = await fetch("/dunbar-users/manage/updateProfileInfo", {
+//             method: 'PUT',
+//             cache: 'default',
+//             headers: {
+//                 'Authorization': `Bearer ${window.DLA_USER?.authorization}`,
+//                 'Content-Type': "application/json; charset=utf-8"
+//             },
+//             body: JSON.stringify(data)
+//         })
+//             .then(r => r.json())
+//             .catch(err => {
+//                 console.error("User Not Updated")
+//                 console.error(err)
+//                 return {}
+//             })
+//         if (updatedUser.user_id) {
+//             alert("User Info Updated!")
+//         }
+//         else {
+//             alert("User Info Update Failed!")
+//         }
+// }
 
-/**
- * Auth0 redirects here with a bunch of info in hash variables.
- * This function allows you pull a single variable from the hash
- */
-function getURLHash(variable, urlString = document.location.href) {
-    const url = new URL(urlString)
-    var vars = new URLSearchParams(url.hash.substring(1))
-    return vars.get(variable) ?? false
-}
+// /**
+//  * Auth0 redirects here with a bunch of info in hash variables.
+//  * This function allows you pull a single variable from the hash
+//  */
+// function getURLHash(variable, urlString = document.location.href) {
+//     const url = new URL(urlString)
+//     var vars = new URLSearchParams(url.hash.substring(1))
+//     return vars.get(variable) ?? false
+// }
 
 /**
  * Use our Auth0 Server back end to ask for all the Dunbap Apps users.
@@ -193,7 +175,8 @@ async function getAllUsers() {
         })
 }
 
-function isAdmin(user) {
+function isAdmin(token) {
+    const user = jwt_decode(token)
     return userHasRole(user, DUNBAR_ADMIN_ROLE)
 }
 /**
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,15 @@
 let express = require('express')
 let router = express.Router()
 
+const staticRouter = require('./static')
+router.get('/',staticRouter)
+
+const manageAPI = require('./manage-api')
+router.get('/',manageAPI)
+
 /* GET home page. */
-router.get('/*', function(req, res, next) {
-  res.sendFile('../manage.html',{root:__dirname})
-  // res.render('index', { title: 'Dunbar Users' })
+router.get('/', function(req, res, next) {
+  res.redirect('/index.html',200)
 })
 
 module.exports = router
