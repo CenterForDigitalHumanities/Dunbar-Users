da9e488750a6e0603f747efb0c9174a8381f5c22
cubap <cubap@slu.edu>
2023-02-21 10:26:30 -0600

    slimming down

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ let express = require('express')
 let router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Dunbar Users' })
+router.get('/*', function(req, res, next) {
+  res.sendFile('../manage.html',{root:__dirname})
+  // res.render('index', { title: 'Dunbar Users' })
 })
 
 module.exports = router
